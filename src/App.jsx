fadd98208a4fb83d532215f27a3027df9db295e5
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-29 00:16:09 -0300

    feat: organize carrousel imgs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./components/Home"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './App.css'
 
 function App() {
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,6 +4,15 @@ import plane_icon from "../assets/img/plane_icon.svg";
 import car_icon from "../assets/img/car_icon.svg"
 import sign_icon from "../assets/img/sign_icon.svg"
 import whats_icon from "../assets/img/whats_icon.svg"
+import translados1 from "../assets/img/tr1.webp"
+import translados2 from "../assets/img/tr2.jpg"
+import translados3 from "../assets/img/tr3.jpg"
+import praia1 from "../assets/img/praia1.jpg"
+import praia2 from "../assets/img/praia2.jpg"
+import praia3 from "../assets/img/praia3.webp"
+import locationIcon from "../assets/img/location_icon.svg"
+
+
 function MainContent() {
     return (  
         <>
@@ -40,7 +49,7 @@ function MainContent() {
 
                         <div className={style.service_card} >
                             <div className={style.card_body}>
-                                <img src={sign_icon} alt="sign_icon" width:auto height="60rem" />
+                                <img src={sign_icon} alt="sign_icon"  width:auto height="60rem" />
                                 <h5 className="card-title text-center">Transfer</h5>
                                 <p className="card-text text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia harum, nam dicta.</p>
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
@@ -59,8 +68,8 @@ function MainContent() {
 
                     <h2 className={style.title_article}>Translados</h2>
                     <div className={style.service_cards_section}>
-                        <div className="card bg-secondary" >
-                            {/* <img src="{% static "home/images/tr1.webp" %}" className="card-img-top" alt="tr1"> */}
+                        <div class="card bg-secondary" style={{width:"18rem"}} >
+                            <img src={translados1} class="card-img-top" alt="tr1" />
                             <div className="card-body">
                                 <h5 className="card-title">Translado 1</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -69,7 +78,7 @@ function MainContent() {
                         </div>
 
                         <div className="card bg-secondary" style={{ width: "18rem" }}>
-                            {/* <img src="{% static "home/images/tr2.jpg" %}" className="card-img-top" alt="tr2"> */}
+                            <img src={translados2} class="card-img-top" alt="tr2" />
                             <div className="card-body">
                                 <h5 className="card-title">Translado 2</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -78,8 +87,8 @@ function MainContent() {
                         </div>
 
                         <div className="card bg-secondary" style={{ width: "18rem" }}>
-                            {/* <img src="{% static "home/images/tr3.jpg" %}" className="card-img-top" alt="tr3"> */}
-                            <div className="card-body">
+                        <img src={translados3} class="card-img-top" alt="tr2" />
+                        <div className="card-body">
                                 <h5 className="card-title">Translado 3</h5>
                                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
@@ -94,53 +103,53 @@ function MainContent() {
                             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div>
-                        <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                {/* <img src="{% static "home/images/praia1.jpg" %}" className="d-block w-100" alt="..."> */}
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src={praia1} className="d-block w-100" alt="praia1"/>
                                 <div className="carousel-caption d-none d-md-block">
                                     <div className={style.beach_description}>
                                         <h5>Praia da daniela</h5>
-                                        {/* <img src="{% static "home/images/location_icon.svg" %}" style={{ height: "30px", width: "30px" }} /> */}
+                                        <img src={locationIcon} style={{ height: "30px", width: "30px" }} />
                                     </div>
                                     <p>Um refúgio para quem busca um lugar tranquilo para relaxar e aproveitar a beleza natural da região</p>
                                 </div>
                             </div>
 
-                            <div className="carousel-item">
-                                {/* <img src="{% static "home/images/praia2.jpg" %}" className="d-block w-100" alt="..."> */}
-                                <div className="carousel-caption d-none d-md-block">
+                            <div class="carousel-item">
+                                <img src={praia2} className="d-block w-100" alt="praia2"/>
+                                <div class="carousel-caption d-none d-md-block">
                                     <div className={style.beach_description}>
                                         <h5>Praia brava</h5>
-                                        {/* <img src="{% static "home/images/location_icon.svg" %}" style={{ height: "30px", width: "30px" }} /> */}
+                                        <img src={locationIcon} style={{ height: "30px", width: "30px" }} />
                                     </div>
                                     <p>Água limpa e cristalina, além de belezas naturais e ótimas ondas para o surf e bodyboarding.</p>
                                 </div>
                             </div>
 
-                            <div className="carousel-item">
-                                {/* <img src="{% static "home/images/praia3.webp" %}" className="d-block w-100" alt="..."> */}
-                                <div className="carousel-caption d-none d-md-block">
+                            <div class="carousel-item">
+                                <img src={praia2} className="d-block w-100" alt="praia2"/>
+                                <div class="carousel-caption d-none d-md-block">
                                     <div className={style.beach_description}>
                                         <h5>Praia do Campeche</h5>
-                                        {/* <img src="{% static "home/images/location_icon.svg" %}" style={{ height: "30px", width: "30px" }} /> */}
+                                        <img src={locationIcon} style={{ height: "30px", width: "30px" }} />
                                     </div>
                                     <p>A praia é procurada por surfistas do mundo inteiro, que encontram as conhecidas "direitas"</p>
                                 </div>
                             </div>
 
                         </div>
-                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Previous</span>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
                         </button>
-                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Next</span>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
                         </button>
                     </div>
 
                     <h2>Faça um orçamento agora!</h2>
-                    <a href="#" className="btn btn-success btn-lg active" role="button" aria-pressed="true">Entre em contato!</a>
+                    <a href="#" class="btn btn-success btn-lg active" role="button" aria-pressed="true">Entre em contato!</a>
                 </div>
             </article>
         </>
@@ -148,3 +157,4 @@ function MainContent() {
 }
 
 export default MainContent;
+
