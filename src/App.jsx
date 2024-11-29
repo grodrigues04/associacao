7ede4b45d61d3fca83f16ca14f4f2fa3c5b5c577
Gustavo Bernardo Rodrigues <gurodrigues.b@gmail.com>
2024-11-29 01:56:12 -0300

    fix: change route to match with html file

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />   
         <Route path="/contato" element={<Contato/>} />
-        <Route path="/associados" element={<Associados/>} />
+        <Route path="/associacao" element={<Associados/>} />
       </Routes>
     </BrowserRouter>
   )
